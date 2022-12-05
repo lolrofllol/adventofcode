15ea8cd44c2150b7c4be5852ee5f530d4cbba52f
Maximilian Koban <info@dofudo.de>
2022-12-05 22:13:10 +0100

    part 2 of 2019/1

--- 2019/01/main.py
+++ 2019/01/main.py
@@ -7,4 +7,17 @@ for line in file:
     fuel_req = int(int(line) / 3) - 2
     sum += fuel_req
 
+    additional_fuel_req = fuel_req
+    module_sum = 0
+
+    while(additional_fuel_req > 0):
+        fuel_iter = int(additional_fuel_req / 3) - 2
+        if(fuel_iter <= 0):
+            fuel_iter = 0
+        print(f'{additional_fuel_req} fuel requires {fuel_iter} more fuel')
+        module_sum += fuel_iter
+        additional_fuel_req = fuel_iter
+
+    sum += module_sum
+
 print(sum)
