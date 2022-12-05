8c0bb772baefce40ee10ef21fee00c2440225a8d
Maximilian Koban <info@dofudo.de>
2022-12-05 23:10:27 +0100

    part 2 of 2019/2

--- 2019/02/main.py
+++ 2019/02/main.py
@@ -1,31 +1,47 @@
 file = open('D:\\Development\\GitHub\\adventofcode\\2019\\02\\input.txt', 'r')
 
 prog_str = file.read().split(',')
-prog_array = [int(x) for x in prog_str]
-pointer = 0
-running = True
-while (running):
-    op_code = prog_array[pointer]
-    storage_pointer_first = prog_array[pointer+1]
-    storage_pointer_second = prog_array[pointer+2]
-    storage_pointer_result = prog_array[pointer+3]
 
-    figure_first = prog_array[storage_pointer_first]
-    figure_second = prog_array[storage_pointer_second]
+desired_output = 19690720
 
-    if op_code == 1:
-        # sum
-        print(f'adding {figure_first} and {figure_second}')
-        prog_array[storage_pointer_result] = figure_first + figure_second
-    elif op_code == 2:
-        # mul
-        print(f'multiplying {figure_first} and {figure_second}')
-        prog_array[storage_pointer_result] = figure_first * figure_second         
-    elif op_code == 99:
-        # halt
-        print(prog_array[0])
-        running = False
-    else:
-        raise Exception('GEHT NICHT')
+brute_force = True
 
-    pointer += 4
+while (brute_force):
+    for noun in range(100):
+        for verb in range(100):
+            print(f'testing noun {noun} and verb {verb}...')
+            prog_array = [int(x) for x in prog_str]
+            prog_array[1] = noun
+            prog_array[2] = verb
+
+            pointer = 0
+            running = True
+
+            while (running):
+                op_code = prog_array[pointer]
+                storage_pointer_first = prog_array[pointer+1]
+                storage_pointer_second = prog_array[pointer+2]
+                storage_pointer_result = prog_array[pointer+3]
+
+                figure_first = prog_array[storage_pointer_first]
+                figure_second = prog_array[storage_pointer_second]
+
+                if op_code == 1:
+                    # sum
+                    # print(f'adding {figure_first} and {figure_second}')
+                    prog_array[storage_pointer_result] = figure_first + figure_second
+                elif op_code == 2:
+                    # mul
+                    # print(f'multiplying {figure_first} and {figure_second}')
+                    prog_array[storage_pointer_result] = figure_first * figure_second
+                elif op_code == 99:
+                    # halt
+                    # print(prog_array[0])
+                    running = False
+                else:
+                    raise Exception('GEHT NICHT')
+
+                pointer += 4
+
+            if (prog_array[0] == desired_output):
+                raise Exception(f'solution found for noun {noun} and verb {verb}')
